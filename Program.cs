f2f98122b5364982425e37b0b46da7da651616ad
Chadwell Clark <wcclark23@comcast.net>
2021-05-21 11:39:54 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+Console.Write("First Name>");
+string firstName = Console.ReadLine();
+
+Console.Write("Last Name>");
+string lastName = Console.ReadLine();
+
+// string name = firstName + " " + lastName;
+string name = ($"{firstName}   {lastName}");
+
+
+for (int i = name.Length; i >= 1; i--)
+{
+    Console.Write(name[i - 1]);
+}
+// Console.WriteLine(lastName.Reverse());
